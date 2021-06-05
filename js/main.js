b9c8e1033146145b262934eb84592118da8ef7d7
Марат <maratok@yandex.ru>
2021-06-05 16:25:35 +0300

    Исправил alert на просто передачу переменной

--- js/main.js
+++ js/main.js
@@ -2,12 +2,11 @@
 const randomNumber = function (number1, number2) {
   if(number1 >= 0){
     if(number2 <= number1){
-      alert('Операция не выполнена. Второе число должно быть больше первого');
+      return Math.round(Math.random() * number1) + number2 ;
     }
     // Функция взята с https://ru.stackoverflow.com/questions/863591/%D0%A1%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B5-%D1%87%D0%B8%D1%81%D0%BB%D0%BE-%D0%BE%D1%82-1-%D0%B4%D0%BE-100
     return Math.round(Math.random() * number2) + number1 ;
   }
-  alert('Числа должны быть положительными');
 };
 randomNumber(1,5);
 
@@ -17,7 +16,6 @@ const countLetters = function(strick, maxLetters){
   if(numberLetters <= maxLetters){
     return numberLetters;
   }
-  alert('Количество букв больше допущенного');
 };
 
 countLetters('Сюдакот', 140);
