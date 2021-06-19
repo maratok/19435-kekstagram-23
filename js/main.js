1a23afe9433923070993fd27183889de58cad95a
Марат <maratok@yandex.ru>
2021-06-19 18:17:49 +0300

    Задание Модуль4-задание1

--- js/main.js
+++ js/main.js
@@ -19,3 +19,63 @@ const countLetters = function(strick, maxLetters){
 };
 
 countLetters('Сюдакот', 140);
+//Начало модуля 4
+const COUNT_PHOTOS = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_NUMBER_AVATAR = 1;
+const MAX_NUMBER_AVATAR = 6;
+const COMMENT_COUNT = 5;
+const arrayMessage = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const arrayNames = [
+  'Эмилия',
+  'София',
+  'Телепузик',
+  'Барсик',
+];
+
+const getRandomArrayElement = (elements) => elements[randomNumber(0, elements.length - 1)];
+const getRandomAvatar = () => `img/avatar-${ randomNumber(MIN_NUMBER_AVATAR, MAX_NUMBER_AVATAR)}.svg`;
+const getPhotoMessages = function(){
+  const numberComments = randomNumber(0,1);
+  if(numberComments === 0){
+    return getRandomArrayElement(arrayMessage);
+  }
+  return `${getRandomArrayElement(arrayMessage)} ${getRandomArrayElement(arrayMessage)}`;
+};
+
+const getComments = function(id) {
+  const array = [];
+  for(let jj = 0; jj < id; jj++){
+    array[jj] = {
+      id: jj+1,
+      avatar: getRandomAvatar(),
+      message: getPhotoMessages(),
+      name: getRandomArrayElement(arrayNames),
+    };
+  }
+  return array;
+};
+
+const getArrayPhotos = function(id){
+  const array = [];
+  for(let ii = 0; ii < id; ii++){
+    array[ii] = {
+      id: ii+1,
+      url: `photos/'${ii+1}.jpg`,
+      description: 'Крутое необычное изящное фото',
+      likes: randomNumber(MIN_LIKES, MAX_LIKES),
+      comments: getComments(COMMENT_COUNT),
+    };
+  }
+  return array;
+};
+
+console.log(getArrayPhotos(COUNT_PHOTOS));
