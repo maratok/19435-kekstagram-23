acc38b7a5e0c9f6f40223aaab134ae72f2c85d49
Марат <maratok@yandex.ru>
2021-07-20 21:41:32 +0300

    исправил часть задания

--- js/create-thumbnails.js
+++ js/create-thumbnails.js
@@ -17,22 +17,25 @@ arrayPhotos.forEach((photo) => {
   pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
   photoFragment.appendChild(pictureElement);
 
-  //Обработчик на открытие большой картинки
-  pictureElement.addEventListener('click', () => {
-
+  //Функции открытия большой картинки
+  const showBigPicture = () => {
     body.classList.add('modal-open');
     bigPicture.classList.remove('hidden');
+    const commentsLength = photo.comments.length;
     const bigPictireImg = bigPicture.querySelector('.big-picture__img');
     bigPictireImg.children[0].src = photo.url;
     const bigPictireLikes = bigPicture.querySelector('.likes-count');
     bigPictireLikes.textContent = photo.likes;
     const bigPictureCommentsLength = bigPicture.querySelector('.comments-count');
-    bigPictureCommentsLength.textContent = photo.comments.length;
+    bigPictureCommentsLength.textContent = commentsLength;
     const bigPictureDescription = bigPicture.querySelector('.social__caption');
     bigPictureDescription.textContent = photo.description;
     const bigPictureComments = bigPicture.querySelector('.social__comments');
     const commentsLoaderButton = bigPicture.querySelector('.social__comments-loader');
     commentsLoaderButton.classList.remove('hidden');
+    const pictureCommentCount = document.querySelector('.social__comment-count');
+    const closePopup = document.querySelector('.big-picture__cancel');
+    let countShowComments = 5;
 
     //Создание блока комментариев
     const createCommentBigPicture = function (photoComments, count) {
@@ -60,69 +63,61 @@ arrayPhotos.forEach((photo) => {
       return commentElements;
     };
 
-    let COUNT_SHOW_COMMENTS = 5;
-
-    //Отрисовка первых пяти комментариев
-    bigPictureComments.appendChild(createCommentBigPicture(photo, COUNT_SHOW_COMMENTS));
-    //Удаления, а потом добавления счетчика комментари
-    const pictureCommentCount = document.querySelector('.social__comment-count');
-    pictureCommentCount.textContent = '';
-    //Число комментариев которые показаны из всех комментариев
-    let countShowCommentsNow = document.querySelectorAll('.social__comment').length;
-    pictureCommentCount.insertAdjacentHTML('afterbegin', `${countShowCommentsNow} из <span class="comments-count">${photo.comments.length}</span> комментариев`);
-
-    //Обработка клика
-    commentsLoaderButton.addEventListener('click', () => {
-      commentsLoaderButton.classList.remove('hidden');
-      //Удаление внтуреенностей Счетчика комментариев
-      pictureCommentCount.textContent = '';
-      //Удаление внтуреенностей блока комментариев
+    const showComments = function(count){
       bigPictureComments.textContent = '';
-      //Новое число загрузки комментариев
-      COUNT_SHOW_COMMENTS += 5;
-      //Если число комментариев для отрисовки больше числа всех комментариев отрисовываем все комментарии
-      if(COUNT_SHOW_COMMENTS > photo.comments.length){
-        const fut = photo.comments.length;
-        bigPictureComments.appendChild(createCommentBigPicture(photo, fut));
+      bigPictureComments.appendChild(createCommentBigPicture(photo, count));
+    };
+    const showCountComments = function(allComments){
+      pictureCommentCount.textContent = '';
+      const countShowCommentsNow = document.querySelectorAll('.social__comment').length;
+      pictureCommentCount.insertAdjacentHTML('afterbegin', `${countShowCommentsNow} из <span class="comments-count">${allComments}</span> комментариев`);
+    };
 
-        countShowCommentsNow = document.querySelectorAll('.social__comment').length;
-        pictureCommentCount.insertAdjacentHTML('afterbegin', `${countShowCommentsNow} из <span class="comments-count">${photo.comments.length}</span> комментариев`);
-        // Если число показанных комментариев равно числу всех комментариев скрываем кнопку
-        if(countShowCommentsNow === photo.comments.length){
+    showComments(countShowComments);
+    showCountComments(commentsLength);
+
+    const loadComments = () => {
+      commentsLoaderButton.classList.remove('hidden');
+      countShowComments += 5;
+      if(countShowComments > commentsLength){
+        showComments(commentsLength);
+        showCountComments(commentsLength);
+        if(commentsLength === commentsLength){
           commentsLoaderButton.classList.add('hidden');
         }
       } else  {
-        //Отрисовка без условия по клику
-        bigPictureComments.appendChild(createCommentBigPicture(photo, COUNT_SHOW_COMMENTS));
-        pictureCommentCount.textContent = '';
-        //считает количество комментариев показанных комментариев
-        countShowCommentsNow = document.querySelectorAll('.social__comment').length;
-        pictureCommentCount.insertAdjacentHTML('afterbegin', `${countShowCommentsNow} из <span class="comments-count">${photo.comments.length}</span> комментариев`);
+        showComments(countShowComments);
+        showCountComments(commentsLength);
       }
-    });
+    };
 
-    //Обрабочтик на клик кретика большой картинки
-    const closePopup = document.querySelector('.big-picture__cancel');
-    closePopup.addEventListener('click', (evt) => {
-      evt.preventDefault();
+    commentsLoaderButton.addEventListener('click', loadComments);
+
+    const hidePopup = () => {
       body.classList.remove('modal-open');
       bigPicture.classList.add('hidden');
       bigPictureComments.innerHTML = '';
-    });
-    //Обрабочтик на клик кнопки Esc во время открытой большой картинки
-    document.addEventListener('keydown', (evt) => {
+      commentsLoaderButton.removeEventListener('click', loadComments);
+    };
+
+    const buttonClickHandler = (evt) => {
+      evt.preventDefault();
+      hidePopup();
+    };
+    const buttonKeydownHandler = (evt) => {
       if (isEscEvent(evt)) {
         evt.preventDefault();
-        body.classList.remove('modal-open');
-        bigPicture.classList.add('hidden');
-        bigPictureComments.innerHTML = '';
+        hidePopup();
       }
-    });
-  });
+    };
 
+    closePopup.addEventListener('click', buttonClickHandler);
+    document.addEventListener('keydown', buttonKeydownHandler);
+  };
+  //Обработчик на открытие большой картинки
+  pictureElement.addEventListener('click', showBigPicture);
 });
 
-//добавления фрагмента с миниатюрами в элемент с классом ".pictures"
 picturesContainer.appendChild(photoFragment);
 
 export {picturesContainer};
